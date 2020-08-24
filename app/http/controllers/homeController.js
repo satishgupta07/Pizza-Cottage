443cc1ff46bbf79dba72fd382d33e18988a17be2
satishgupta07 <satishgupta.cse01@gmail.com>
2020-08-24 18:19:40 +0530

    Showing fetched data on frontend
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

--- app/http/controllers/homeController.js
+++ app/http/controllers/homeController.js
@@ -8,10 +8,6 @@ function homeController() {
             console.log(pizzas)
             return res.render('home', {pizzas : pizzas})
 
-            // Menu.find().then(function(pizzas) {
-            //     console.log(pizzas)
-            //     return res.render('home', {pizzas : pizzas})
-            // })
         }
     }
 }
