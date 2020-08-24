0490629e0e319ec15195f3b09d64c9886003b3c0
satishgupta07 <satishgupta.cse01@gmail.com>
2020-08-24 22:38:26 +0530

    Build add to cart functionality
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

--- server.js
+++ server.js
@@ -47,6 +47,13 @@ app.use(flash())
 // Assets
 app.use(express.static('public'))
 
+app.use(express.json())
+
+// Global middleware 
+app.use((req, res, next) => {
+  res.locals.session = req.session
+  next()
+})
 
 // Set Template Engine
 app.use(expressLayout)
