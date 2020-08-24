0e1abe4b7280045ae51d7012a03619d226ae0b7a
satishgupta07 <satishgupta.cse01@gmail.com>
2020-08-24 20:42:38 +0530

    Created Session and Cookies
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

--- server.js
+++ server.js
@@ -1,22 +1,48 @@
+require('dotenv').config()
 const express = require('express')
 const ejs = require('ejs')
 const expressLayout = require('express-ejs-layouts')
 const path = require('path')
 const mongoose = require('mongoose')
+const session = require('express-session')
+const flash = require('express-flash')
+const MongoDbStore = require('connect-mongo')(session)
 
 const app = express()
 
 const PORT = process.env.PORT || 3000
 
+
+// Database Connection
+
 const {mongoURI} = require("./mongo_config/key");
 
 mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology:true});
-mongoose.connection.once('open', function(){
+const connection = mongoose.connection;
+connection.once('open', function(){
   console.log('Conection has been made!');
 }).on('error', function(error){
     console.log('Error is: ', error);
 });
 
+
+// Session store 
+let mongoStore = new MongoDbStore({
+  mongooseConnection: connection,
+  collection: 'sessions'
+})
+
+// Session config
+
+app.use(session({
+  secret: process.env.COOKIE_SECRET,
+  resave: false, 
+  store: mongoStore,
+  saveUninitialized: false, 
+  cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24 hour 
+}))
+
+app.use(flash())
 
 // Assets
 app.use(express.static('public'))
